89a5e69777d1964589272e16b5bd50f154478e17
rayraisu <59055359+raisuray@users.noreply.github.com>
2021-11-28 21:46:10 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+if __name__ == '__main__':
+    file_name = '1991074679.txt'
+
+    with open(file_name + 'r') as f:
+        doc = f.readlines
+
+    
---
+++ tools.py
@@ -0,0 +1,139 @@
+import spacy
+import re
+import neologdn
+import json
+
+from IGNORE_WORDS import ig
+
+def find_noun_and_compound(doc, i):
+    
+    found_compound_noun = 0
+    count = 0
+    compound = ''
+    maxword = len(doc)
+
+    list_of_compound_word = set()
+    list_of_noun_word = set()
+    list_all_words = set()
+
+    while True:
+
+        """
+        try:    
+            if(doc[count].pos_ == "NUM" and doc[count+1].dep_ == "compound"):
+                compound += doc[count].text
+                count += 1
+        except IndexError:
+            print("Index out of limit! Line : " + str(i) + " 気にしないで")
+        """
+        norm = neologdn.normalize(doc[count].norm_).isascii 
+        while doc[count].dep_ == "compound" and doc[count].pos_ == "NOUN" and (norm != True):
+            text = doc[count].text
+            if (text in ig):
+                break
+            compound += text
+            count += 1
+            found_compound_noun = 1
+
+        
+        if(found_compound_noun == 1 and doc[count].pos_ == "NOUN"):
+            if(doc[count].text in ig ):
+                compound = ''
+                found_compound_noun = 0                
+            else :
+                compound += doc[count].text
+                found_compound_noun = 0
+                list_of_compound_word.add(compound)
+                #print(compound)
+                compound = ""
+       
+        else:
+            compound = ''
+            found_compound_noun = 0
+        
+        """
+        if(doc[count].pos_ == "NOUN" and found_compound_noun != 1):
+            if(doc[count].text not in ig):
+                list_of_noun_word.add(doc[count].text)
+        """
+        count += 1
+
+        if(maxword == count):
+            break
+
+    for word in list_of_compound_word:
+        list_all_words.add(word)
+
+    for word in list_of_noun_word:
+        list_all_words.add(word)
+
+    list_of_compound_word = list(list_of_compound_word)
+    list_of_noun_word = list(list_of_noun_word)
+    list_all_words = list(list_all_words)
+
+    return list_all_words, list_of_compound_word, list_of_noun_word
+
+def exct_experimental_section(doc):
+
+    start   = 0
+    end     = 0
+    read    = 0
+    pattern_start = re.compile("【実施例.*】|（実施例.*）")
+    pattern_end  = re.compile("【発明.*】|（発明.*）|【図面の簡単な.*】")
+
+    exp_texts = []
+
+    for i, experiment in enumerate(doc):
+        if(read == 0 and pattern_start.match(experiment) != None):   
+            read = 1
+            start = i
+        if(read == 1 and pattern_end.match(experiment) != None):
+            end = i
+            break
+
+    exp_texts = doc[start:end]
+
+    for i in range(len(exp_texts)):
+        exp_texts[i] = re.sub(r"【.*】|（.*） ?|〔[１２３４５６７８９０]?〕|<.*>|〈.*〉", "", exp_texts[i])
+        exp_texts[i] = re.sub("\n| ?", "",exp_texts[i])
+        exp_texts[i] = neologdn.normalize(exp_texts[i])
+
+    while "" in exp_texts:
+        exp_texts.remove("")
+
+    #pprint.pprint(exp_texts)
+    return exp_texts
+
+def make_one(out):
+    out["list_all_words"] = list(set( out["list_all_words"]))
+    out["list_of_compound_word"] = list(set(out["list_of_compound_word"]))
+    out["list_of_noun_word"] = list(set(out["list_of_noun_word"]))
+    return out
+
+if __name__ == '__main__':
+    
+    nlp = spacy.load("ja_ginza")
+    
+
+    file_name = '1994254512.txt'
+    out = {'list_all_words':[], 'list_of_compound_word':[], 'list_of_noun_word':[]}
+    
+    with open('effect_words/'+file_name ,mode='r') as f:
+        doc = f.readlines()
+
+    doc = exct_experimental_section(doc)
+
+    for i in range(len(doc)):
+        doc_samp = nlp(doc[i])
+        list_all_words, list_of_compound_word, list_of_noun_word = find_noun_and_compound(doc_samp, i)
+        out["list_all_words"].extend(list_all_words)
+        out["list_of_compound_word"].extend(list_of_compound_word)
+        out["list_of_noun_word"].extend(list_of_noun_word)
+    out = make_one(out)
+    out_final = {file_name:out}
+    with open("out.json", 'w') as f:
+        json.dump(out_final,f, indent=4, ensure_ascii=False)
+    
+
+
+   
