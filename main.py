9d50a34bac707ff6c837705e6d7f2ce79eaf9077
raisuray <raymond.widjaja10@gmail.com>
2021-11-30 21:49:39 +0900

    add 例外

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ import os
 import spacy
 import tools
 import json 
+import pprint
 
 
 path = './effect_words/'
 effect_words = os.listdir(path)
-dict_eff_words = dict.fromkeys(effect_words, [])
+dict_eff_words = dict.fromkeys(effect_words)
 
 
 patents_list = []
@@ -24,13 +25,21 @@ for patent in dict_eff_words.keys():
 #FIND ALL THE COMPOUND WORD IN ALL FILE#
 nlp = spacy.load("ja_ginza")
 for patent in patents_list:
+    res = []
+    list_of_compound_word = []
     print(patent.name)
+    #print(len(patent.doc))
 
     for i in range(len(patent.doc)):
+        
         doc_samp = nlp(patent.doc[i])
+        if(tools.check_symbol(doc_samp) == True and len(patent.doc[i]) <= 10):
+            continue
         list_of_compound_word = tools.find_noun_and_compound(doc_samp, i)
-    
-    dict_eff_words[patent.name].extend(list_of_compound_word)
+        res.extend(list_of_compound_word)        
+    res = list(set(res))
+    dict_eff_words[patent.name] = res
+
 
 #MAKE AN OUTPUT FILE#
 with open("out.json", 'w') as f:
